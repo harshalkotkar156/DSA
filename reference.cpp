7801635adebe19ea9532bf96f7d529c99e84af2a
harshalkotkar156 <harshalkotkar156@gmail.com>
2024-02-05 09:08:35 +0530

    reference added

---
+++ reference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void print(int &g)
+{
+    cout<<(g)<<endl;
+}
+int main()
+{
+    long long n=433356654545;
+    cout<<"Start\n";
+    cout<<sizeof(n)<<endl;
+    cout<<"address : "<<(&n)<<endl;
+    return 0;
+}
